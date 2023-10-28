183cb8643e258d60595a8c8447e769ea150fa6de
Mgalicka <j.malgosia1@wp.pl>
2023-10-28 17:35:01 +0200

    thir commit

--- assets/fetchCountries.js
+++ assets/fetchCountries.js
@@ -1,14 +1,18 @@
-module.exports = function fetchCountries(name) {
-  return fetchCountries(name)
-    .then(countries => {
-      
-      console.log('Kraje:', countries);
-   
-      return countries;
-    })
-    .catch(error => {
-     
-      console.error('Błąd:', error);
-      throw error;
-    });
+export const fetchCountries = (name) => {
+  return fetch(`https://restcountries.com/v3.1/name/${name}`)
+      .then(response => {
+          if (!response.ok) {
+              throw new Error('Network response was not ok');
+          }
+          return response.json();
+      })
+      .then(data => {
+        return data;
+      })
+      .catch(error => {
+          console.error('There has been a problem with your fetch operation:', error);
+      });
 };
+
+
+
